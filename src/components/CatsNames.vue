34180e58f04a02ecc766d735056c199ab3a2d2fb
Jesus <jesus.donoso@sngular.com>
2023-05-01 19:38:36 +0200

    Add CatsNames component

---
+++ src/components/CatsNames.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="item-names">
+    <h2>Items</h2>
+    <ul>
+      <li v-for="item in items" :key="item.id" @click="clickItem(item)">
+        {{ item.id }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'CatsNames',
+    props: {
+      items: Array
+    },
+    methods: {
+      clickItem(item) {
+        this.$emit('clickItem', item);
+      }
+    }
+}
+</script>
