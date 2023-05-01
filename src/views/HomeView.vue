a9317bb9ff752cbcd45e820008a79a3874ae9f65
Jesus <jesus.donoso@sngular.com>
2023-05-01 19:38:51 +0200

    Add Home View and styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,45 @@
 <template>
-    <div>
-        Bienvenido a la Home
+    <div class="container">
+        <div class="left-side">
+            <CatsNames :items="listImages" @clickItem="showItem" />
+        </div>
+        <div class="right-side">
+            <CatDetails :item="selectedCat"/>
+        </div>
     </div>
 </template>
+<script>
+import CatsNames from '@/components/CatsNames.vue'
+import CatDetails from '@/components/CatDetails.vue'
+import { images } from '@/api';
+
+export default {
+    name: 'HomeView',
+    components: {
+        CatsNames,
+        CatDetails
+    },
+    data() {
+        return {
+            listImages: [],
+            selectedCat: {}
+        }
+    },
+    methods: {
+        async fetchListImages() {
+            const list = await images.list();
+            let listCats = list.map((item) => {return {id: item.id, url: item.url, timesClicked: 0}})
+            this.$set(this, 'listImages', listCats)
+        },
+        showItem(item) {
+            this.$set(this, 'selectedCat', item);
+        },
+
+    },
+
+    mounted() {
+        this.fetchListImages();
+    }
+}
+
+</script>
