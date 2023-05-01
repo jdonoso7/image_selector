f26248afe20af3024cd411e95eb366d16a517338
Jesus <jesus.donoso@sngular.com>
2023-05-01 19:38:20 +0200

    Add CatDetails component

---
+++ src/components/CatDetails.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="item-details" v-if="item && item.id">
+    <h2>{{ item.id }}</h2>
+    <img :src="item.url" @click="(incrementClicks(item))" />
+    <p>Clicks: {{ item.timesClicked }}</p>
+  </div>
+  <div class="no-item-selected" v-else>
+    <p>No item selected</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'CatDetails',
+    props: {
+        item: Object
+    },
+    methods: {
+        incrementClicks(item) {
+            item.timesClicked = item.timesClicked +1;
+        }
+    }
+}
+</script>
